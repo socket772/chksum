7067cb9673c0ebb0d24020e6336f80cc040c3b53
socket772 <socket772@protonmail.com>
2024-10-16 14:10:10 +0200

    Primo test

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use checksums::{self, hash_file, Algorithm};
+use std::env::args;
+use walkdir::WalkDir;
+
+fn main() {
+    // args[1] = cartella
+    // args[2] = file output
+    let args: Vec<String> = args().collect();
+
+    if args.len() != 3 {
+        return;
+    }
+
+    let walk_dir = WalkDir::new(args[1].clone()).follow_links(true);
+
+    for file in walk_dir {
+        let file_path = file.unwrap();
+        let file_checksum = hash_file(file_path.path(), Algorithm::CRC32);
+        println!("{} {:?}", file_checksum, file_path.path())
+    }
+}
