41473ee15b64ac64c17dabb5dfc22a7673c56d50
socket772 <socket772@protonmail.com>
2024-10-17 14:38:26 +0200

    cambiata lettura, tempo in ms, statistiche più precise

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use md5::{Digest, Md5};
-use std::{env::args, fs::read_to_string, time::Instant};
+use std::{env::args, fs::File, io::BufReader, time::Instant};
 use walkdir::WalkDir;
 
 // https://reveng.sourceforge.io/crc-catalogue/all.htm
@@ -20,40 +20,51 @@ fn main() {
     // Avvia time benchmark
     let now = Instant::now();
 
+    println!("Parte programma");
     cycle(walk_dir);
 
     // Fine benchmark
-    let elapsed_time = now.elapsed();
     println!(
-        "Il calcolo dei checksum ha impiegato {} secondi.",
-        elapsed_time.as_secs()
+        "Il calcolo dei checksum ha impiegato {} millisecondi.",
+        now.elapsed().as_millis()
     );
 }
 
 fn cycle(walk_dir: WalkDir) {
     let mut successi: u32 = 0;
+    let mut cartella: u32 = 0;
     let mut errori: u32 = 0;
     for file in walk_dir {
         if file.is_ok() {
             let mut hasher = Md5::new();
-            let file_path = file.unwrap();
-            let file_string = read_to_string(file_path.path());
-            if file_string.is_ok() {
-                hasher.update(file_string.unwrap());
-                let hash = hasher.finalize();
-                if file_path.path().is_file() && file_path.path().exists() {
-                    println!("{:?} {:?}", hash, file_path.path());
+            let file_direntry = file.unwrap();
+
+            if file_direntry.path().exists() {
+                let file_open = File::open(file_direntry.path());
+                if file_direntry.path().is_file() && file_open.is_ok() {
+                    let reader = BufReader::new(file_open.unwrap());
+
+                    hasher.update(reader.buffer());
+                    let hash = hasher.finalize();
+
+                    println!("{:?} {:?}", hash, file_direntry.path());
                     successi += 1;
+                } else if file_direntry.path().is_dir() {
+                    println!("{:?} è una cartella", file_direntry.path());
+                    cartella += 1;
+                } else {
+                    println!("Errore");
+                    errori += 1;
                 }
             } else {
-                // println!("Errore lettura del file o è una cartella -> {:?}", file_path);
+                println!("Non esiste");
                 errori += 1;
             }
-        } else {
-            // println!("Errore, file o cartella non esiste");
-            errori += 1;
         }
     }
 
-    println!("Successi {}, Errori {}", successi, errori);
+    println!(
+        "Successi {}, Cartelle {}, Errori {}",
+        successi, cartella, errori
+    );
 }
